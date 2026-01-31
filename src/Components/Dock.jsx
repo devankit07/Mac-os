b87c5cc51355a7e03cdfc749d9bfb644d18b599d
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-01-31 17:36:09 +0530

    svg added

--- src/Components/Dock.jsx
+++ src/Components/Dock.jsx
@@ -2,7 +2,17 @@
 
 const Dock = () => {
   return (
-    <div>Dock</div>
+    <footer>
+        <div className="icon"><img src="/nav-icons/calendar.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/cli.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/github.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/link.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/mail.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/note.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/pdf.svg" alt="" /></div>
+        <div className="icon"><img src="/nav-icons/spotify.svg" alt="" /></div>
+
+    </footer>
   )
 }
 
