f450a2477ed213f2793c1b449c4c9b13904e9298
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-01 19:09:05 +0530

    svg,cv,&note

--- src/Components/Dock.jsx
+++ src/Components/Dock.jsx
@@ -1,17 +1,16 @@
-
+import "../dock.scss"
 
 const Dock = () => {
   return (
-    <footer>
-        <div className="icon"><img src="/nav-icons/calendar.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/cli.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/github.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/link.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/mail.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/note.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/pdf.svg" alt="" /></div>
-        <div className="icon"><img src="/nav-icons/spotify.svg" alt="" /></div>
-
+    <footer className="dock">
+        <div className="icon"><img src="/doc-icons/calender.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/cli.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/github.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/link.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/mail.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/note.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/pdf.svg" alt="" /></div>
+        <div className="icon"><img src="/doc-icons/spotify.svg" alt="" /></div>
     </footer>
   )
 }
