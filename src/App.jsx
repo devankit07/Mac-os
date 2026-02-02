c4a076353c113e109d167dd58373e9703767a9ef
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-02 17:02:49 +0530

    logic begins

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./app.scss"
 import Dock from "./Components/Dock"
 import Nav from "./Components/Nav"
+import Macwindow from "./windows/macwindow"
+
 
 const App = () => {
   return (
     <main>
       <Nav/>
       <Dock/>
+
+      <Macwindow/>
     </main>
   )
 }
---
+++ src/windows/Macwindow.jsx
@@ -0,0 +1,13 @@
+import { Rnd } from "react-rnd"
+
+const Macwindow = () => {
+  return (
+    <div>
+      <Rnd>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cupiditate repellat minima provident, suscipit numquam!
+      </Rnd>
+    </div>
+  )
+}
+
+export default Macwindow
