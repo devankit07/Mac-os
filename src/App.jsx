4cdd656032232fd1ffd7413cdecd1a504c5b9f19
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-02 15:56:12 +0530

    dock setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./app.scss"
 import Dock from "./Components/Dock"
+import Nav from "./Components/Nav"
 
 const App = () => {
   return (
     <main>
+      <Nav/>
       <Dock/>
     </main>
   )
