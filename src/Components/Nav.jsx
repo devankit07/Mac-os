6c4843098602c5712e671892538cf33b1e864887
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-02 16:44:10 +0530

    nav done

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,32 @@
+import DateTime from "./DateTime";
+
+import("./nav.scss");
+
+const Nav = () => {
+  return (
+    <nav>
+    <div className="left">
+        <div className="apple-icon">
+            <img src="/navbar-icons/apple.svg" alt="apple" />
+        </div>
+        <div className="nav-item"><p>Ankit</p></div>
+        <div className="nav-item"><p>File</p></div>
+        <div className="nav-item"><p>Edit</p></div>
+        <div className="nav-item"><p>Window</p></div>
+        <div className="nav-item"><p>Terminal</p></div>
+
+    </div>
+    <div className="right">
+        <div className="nav-icon">
+            <img src="/navbar-icons/wifi.svg" alt="wifi" />
+        </div>
+        <div className="nav-item">
+            <DateTime/>
+        </div>
+
+    </div>
+    </nav>
+  )
+}
+
+export default Nav
